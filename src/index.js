e872a0c34a339ddf27740887fa21665228851f12
Jesse Shaw <JesseShawCodes@gmail.com>
2020-01-07 19:37:18 -0500

    Added Redux Functionality

--- src/index.js
+++ src/index.js
@@ -1,159 +1,49 @@
-import React from 'react';
-import { render } from 'react-dom';
+import C from './constants'
+import appReducer from './store/reducers'
+// import initialState from './initialState.json'
+import { applyMiddleware, createStore } from 'redux';
+import logger from 'redux-logger';
 
-import Library from './library/Library.js';
+const initialState = (localStorage['redux-store']) ?
+    JSON.parse(localStorage['redux-store']) :
+    {}
 
-//Redux Actions
-import C from './constants';
+const store = createStore(
+    appReducer,
+    initialState,
+    applyMiddleware(logger)
+);
 
-//Import Reducers
-import { goal, skiDay, errors, allSkiDays } from './store/reducers';
+window.store = store
 
-/*Fetch Resort Names*/
-import { fetchResortNames} from './challenges/FETCH_RESORT_NAMES';
-import { cancelFetching } from './challenges/CANCEL_FETCHING';
-import { clearSuggestions } from './challenges/CLEAR_SUGGESTIONS';
-import { changeSuggestions } from './challenges/CHANGE_SUGGESTIONS';
+store.subscribe(() => {
+    const state = JSON.stringify(store.getState())
+    localStorage['redux-store'] = state
+})
 
+store.dispatch({
+    type: C.ADD_DAY,
+    payload: {
+        "resort": "Mt Shasta",
+        "date": "2016-10-28",
+        "powder": false,
+        "backcountry": true
+    }
+})
 
-fetchResortNames();
-cancelFetching();
-clearSuggestions();
-changeSuggestions();
+store.dispatch({
+    type: C.SET_GOAL,
+    payload: 2
+})
+const unsubscribeGoalLogger = store.subscribe(() => console.log(`Goal: ${store.getState().goal}`));
 
+setInterval(() => {
+    store.dispatch({
+        type: C.SET_GOAL,
+        payload: Math.floor(Math.random() * 100)
+    })
+}, 250)
 
-/*
-import { allSkiDays, goal } from './InitState.json';
-
-console.log(`
-    Ski Day Counter
-    ==================
-    The goal is ${goal}
-    Initially there are all ${allSkiDays.length} ski days in state
-
-    Constants (actions)
-    ------------------
-    ${Object.keys(C).join('\n    ')}
-
-`)
-*/
-
-
-//All Ski Days
-// const state = [
-//     {
-//         "resort": "Kirkwood",
-//         "date": "2018-12-15",
-//         "powder": true,
-//         "backcountry": false
-//     }
-// ]
-
-// const action = {
-//     type: C.REMOVE_DAY,
-//     payload: {
-//         "resort": "Kirkwood",
-//         "date": "2018-12-15",
-//         "powder": true,
-//         "backcount": false
-//     }
-// }
-
-// const nextState = allSkiDays(state, action)
-
-// console.log(`
-//     initial state: ${JSON.stringify(state)}
-//     action: ${JSON.stringify(action)}
-//     new state: ${JSON.stringify(nextState)}
-// `)
-
-// const state = 10;
-
-// const action = {
-//     type: C.ADD_DAY,
-//     payload: {
-//         "resort": "Heavenly",
-//         "date": "2016-12-16",
-//         "powder": true,
-//         "backcountry": false
-//     }
-// }
-
-// const nextState = skiDay(state, action);
-
-// console.log( `
-//     initial goal: ${state}
-//     action: ${JSON.stringify(action)}
-//     new goal: ${JSON.stringify(nextState)}
-// `)
-
-const bookList = [
-    {
-        "title": "Title 1",
-        "author": "Author 1",
-        "pages": 260
-    },
-    {
-        "title": "Title 2",
-        "author": "Author 2",
-        "pages": 260
-    },
-    {
-        "title": "Title 3",
-        "author": "Author 3",
-        "pages": 260
-    } 
-]
-
-/*Error Reducers*/
-
-// const state = [
-//     "User Not Authorized",
-//     "Server Feed Not Found"
-// ]
-
-// const action = {
-//     type: C.CLEAR_ERROR,
-//     payload: 0
-// }
-
-// const nextState = errors(state, action);
-
-// console.log(`
-//     initial state: ${state}
-//     action: ${JSON.stringify(action)}
-//     new state: ${JSON.stringify(nextState)}
-// `)
-
-// class FavoriteColorForm extends React.Component {
-//     state = {
-//         value: ''
-//     }
-
-//     newColor = (e) => 
-//         this.setState({ value: e.target.value })
-
-//     submit = e => {
-//         console.log(`New Color: ${this.state.value}`);
-//         e.preventDefault();
-//     }
-//     render() {
-//         return (
-//             <form onSubmit={this.submit}>
-//                 <label>Favorite Color
-//                     <input 
-//                         type="color"
-//                         onChange={this.newColor}
-//                     />
-//                 </label>
-//                 <button>Submit</button>
-//             </form>
-//         )
-//     }
-// }
-
-render(
-    <Library books={bookList} />,
-    // <FavoriteColorForm />,
-    document.getElementById('root')
-)
+setTimeout(() => {
+    unsubscribeGoalLogger();
+}, 3000)
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,87 +1,102 @@
 import C from '../constants'
+import { combineReducers } from 'redux'
 
-// export const goal = (state=10, action) => {
-//     console.log("Goal Reducer is Running");
-//     if (action.type === C.SET_GOAL) {
-//         return action.payload
-//     }
-//     else {
-//         return state
-//     }
-// }
 
-export const skiDay = (state=null, action) => {
-    if (action.type === C.ADD_DAY) {
-        return action.payload
-    }
-    else {
-        return state
-    }
-}
+
+export const goal = (state=10, action) => 
+	(action.type === C.SET_GOAL) ? 
+		 parseInt(action.payload) :
+		 state
+
+export const skiDay = (state=null, action) => 
+  (action.type === C.ADD_DAY) ?
+  	action.payload :
+  	state
 
 export const errors = (state=[], action) => {
-    if (action.type === C.ADD_ERROR) {
-        return [
-            ...state,
-            action.payload
-        ]
-    }
-    if (action.type === C.CLEAR_ERROR) {
-        return state.filter((message, i) => i !== action.payload);
-    }
-    else {
-        return state
-    }
+  switch(action.type) {
+    case C.ADD_ERROR :
+    	return [
+         ...state,
+         action.payload
+    	]
+    case C.CLEAR_ERROR : 
+      return state.filter((message, i) => i !== action.payload)
+  	default: 
+  		return state
+  }
 }
-
-export const goal = (state=10, action) =>
-    (action.type === C.SET_GOAL) ?
-        parseInt(action.payload) : 
-        state
 
 export const allSkiDays = (state=[], action) => {
-    switch(action.type) {
-        case C.ADD_DAY : 
-            console.log(action.payload.date);
-            console.log(state);
-            const hasDayAlready = state.some(skiDay => skiDay.date === action.payload.date)
-            return (hasDayAlready) ? 
-                state : 
-                [
-                    ...state,
-                    skiDay(null, action) 
-                ]
-        case C.REMOVE_DAY :
-            console.log("REMOVE DAY")
-            return state.filter(skiDay => skiDay.date !== action.payload)
-        default:
-            return state
-    }
+
+  switch(action.type) {
+
+    case C.ADD_DAY : 
+
+      const hasDay = state.some(skiDay => skiDay.date === action.payload.date)
+
+      return (hasDay) ?
+         state :
+         [
+           ...state,
+           skiDay(null, action)
+         ].sort((a, b) => new Date(b.date) - new Date(a.date))
+
+    case C.REMOVE_DAY :
+
+      return state.filter(skiDay => skiDay.date !== action.payload)     
+
+    default:
+      return state
+  }
+
 }
 
-export const fetching = (state, action) => {
-    console.log("FETCHING");
-    switch(action.type) {
-        case C.FETCH_RESORT_NAMES:
-            return true
-        case C.CANCEL_FETCHING:
-            return false
-        case C.CHANGE_SUGGESTIONS:
-            return false
-        default:
-            return state
-    }
+export const fetching = (state=false, action) => {
+
+  switch(action.type) {
+
+    case C.FETCH_RESORT_NAMES :
+      return true
+
+    case C.CANCEL_FETCHING :
+      return false 
+
+    case C.CHANGE_SUGGESTIONS :
+      return false   
+
+    default:
+      return state
+  }
+
 }
 
-export const suggestions = (state, action) => {
-    console.log("suggestions reducer running");
-    console.log(action.type);
-    switch(action.type) {
-        case (C.CLEAR_SUGGESTIONS) :
-            return [];
-        case (C.CHANGE_SUGGESTIONS) :
-            return action.payload;
-        default : 
-            return state;
-    }
+export const suggestions = (state=[], action) => {
+
+  switch(action.type) {
+
+    case C.CLEAR_SUGGESTIONS :
+      return []
+
+    case C.CHANGE_SUGGESTIONS :
+      return action.payload  
+
+    default :
+      return state
+  }
+
 }
+
+export default combineReducers({
+  allSkiDays,
+  goal,
+  errors,
+  resortNames: combineReducers({
+    fetching,
+    suggestions
+  })
+})
+
+
+
+
